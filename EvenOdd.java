f144955d7063504d16ac9aab4173ae1e02817a91
Jitendra Singh19 <145744577+jitendrasingh19@users.noreply.github.com>
2024-02-04 17:32:41 +0530

    Update EvenOdd.java

--- EvenOdd.java
+++ EvenOdd.java
@@ -3,10 +3,12 @@ import java.util.Scanner;
 class EvenOdd  
 {  
 public static void main (String args[])  
-{     
+{  
+// sc is the object of Scanner class
 Scanner sc=new Scanner(System.in);  
 System.out.print("Enter the number: ");   
 int num=sc.nextInt();  
+//method
 findEvenOdd(num);  
 }  
 public static void findEvenOdd(int num)  
